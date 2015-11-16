104754aad42b07a3c61809cc72217b77d037b429
juldebar <juldebar@gmail.com>
2015-11-16 16:11:14 +0100

    fonction RDF Commentée

--- R/Atlas_i6_SpeciesMap_julien.R
+++ R/Atlas_i6_SpeciesMap_julien.R
@@ -171,93 +171,93 @@ Atlas_i6_SpeciesMap <- function(df,
     #     regions2=RJSONIO::fromJSON(json2)
     #    
     
-    
-    json = '{"type":"FeatureCollection","features":[
-{"type":"Feature",
-    "properties":{"region_id":1, "region_name":"Australian Alps"},
-    "geometry":{"type":"Polygon","coordinates":[[[141.13037109375,-38.788345355085625],[141.13037109375,-36.65079252503469],[144.38232421875,-36.65079252503469],[144.38232421875,-38.788345355085625],[141.13037109375,-38.788345355085625]]]}},
-{"type":"Feature",
-    "properties":{"region_id":4, "region_name":"Shark Bay"},
-    "geometry":{"type":"Polygon","coordinates":[[[143.10791015625,-37.75334401310656],[143.10791015625,-34.95799531086791],[146.25,-34.95799531086791],[146.25,-37.75334401310656],[143.10791015625,-37.75334401310656]]]}}
-    ]}'
-    regions=RJSONIO::fromJSON(json)
-    lmap <- Leaflet$new()
-    lmap$tileLayer(provide='Stamen.TonerLite')
-    lmap$setView(c(-37, 145), zoom = 1)
-    lmap$geoJson(
-      regions, 
-      style = "#! function(feature) {
-      var rgn2col = {1:'red',2:'blue',4:'green'};     
-      return {
-      color: rgn2col[feature.properties['region_id']],
-      strokeWidth: '1px',
-      strokeOpacity: 0.5,
-      fillOpacity: 0.2
-      }; } !#",
-      onEachFeature = "#! function (feature, layer) {
-      
-      // info rollover
-      if (document.getElementsByClassName('info leaflet-control').length == 0 ){
-      info = L.control({position: 'topright'});  // NOTE: made global b/c not ideal place to put this function
-      info.onAdd = function (map) {
-      this._div = L.DomUtil.create('div', 'info');
-      this.update();
-      return this._div;
-      };
-      info.update = function (props) {
-      this._div.innerHTML = '<h4>Field Name</h4>' +  (props ?
-      props['region_id'] + ': <b> + props[fld] + </b>'
-      : 'Hover over a region');
-      };
-      info.addTo(map);
-      };
-      
-      // mouse events
-      layer.on({
-      
-      // mouseover to highlightFeature
-      mouseover: function (e) {
-      var layer = e.target;
-      layer.setStyle({
-      strokeWidth: '3px',
-      strokeOpacity: 0.7,
-      fillOpacity: 0.5
-      });
-      if (!L.Browser.ie && !L.Browser.opera) {
-      layer.bringToFront();
-      }
-      info.update(layer.feature.properties);
-      },
-      
-      // mouseout to resetHighlight
-      mouseout: function (e) {
-      geojsonLayer.resetStyle(e.target);
-      info.update();
-      },
-      
-      // click to zoom
-      click: function (e) {
-      var layer = e.target;        
-      if ( feature.geometry.type === 'MultiPolygon' ) {        
-      // for multipolygons get true extent
-      var bounds = layer.getBounds(); // get the bounds for the first polygon that makes up the multipolygon
-      // loop through coordinates array, skip first element as the bounds var represents the bounds for that element
-      for ( var i = 1, il = feature.geometry.coordinates[0].length; i < il; i++ ) {
-      var ring = feature.geometry.coordinates[0][i];
-      var latLngs = ring.map(function(pair) {
-      return new L.LatLng(pair[1], pair[0]);
-      });
-      var nextBounds = new L.LatLngBounds(latLngs);
-      bounds.extend(nextBounds);
-      }
-      map.fitBounds(bounds);
-      } else {
-      // otherwise use native target bounds
-      map.fitBounds(e.target.getBounds());
-      }
-      }
-      });
-      } !#")
+#     
+#     json = '{"type":"FeatureCollection","features":[
+# {"type":"Feature",
+#     "properties":{"region_id":1, "region_name":"Australian Alps"},
+#     "geometry":{"type":"Polygon","coordinates":[[[141.13037109375,-38.788345355085625],[141.13037109375,-36.65079252503469],[144.38232421875,-36.65079252503469],[144.38232421875,-38.788345355085625],[141.13037109375,-38.788345355085625]]]}},
+# {"type":"Feature",
+#     "properties":{"region_id":4, "region_name":"Shark Bay"},
+#     "geometry":{"type":"Polygon","coordinates":[[[143.10791015625,-37.75334401310656],[143.10791015625,-34.95799531086791],[146.25,-34.95799531086791],[146.25,-37.75334401310656],[143.10791015625,-37.75334401310656]]]}}
+#     ]}'
+#     regions=RJSONIO::fromJSON(json)
+#     lmap <- Leaflet$new()
+#     lmap$tileLayer(provide='Stamen.TonerLite')
+#     lmap$setView(c(-37, 145), zoom = 1)
+#     lmap$geoJson(
+#       regions, 
+#       style = "#! function(feature) {
+#       var rgn2col = {1:'red',2:'blue',4:'green'};     
+#       return {
+#       color: rgn2col[feature.properties['region_id']],
+#       strokeWidth: '1px',
+#       strokeOpacity: 0.5,
+#       fillOpacity: 0.2
+#       }; } !#",
+#       onEachFeature = "#! function (feature, layer) {
+#       
+#       // info rollover
+#       if (document.getElementsByClassName('info leaflet-control').length == 0 ){
+#       info = L.control({position: 'topright'});  // NOTE: made global b/c not ideal place to put this function
+#       info.onAdd = function (map) {
+#       this._div = L.DomUtil.create('div', 'info');
+#       this.update();
+#       return this._div;
+#       };
+#       info.update = function (props) {
+#       this._div.innerHTML = '<h4>Field Name</h4>' +  (props ?
+#       props['region_id'] + ': <b> + props[fld] + </b>'
+#       : 'Hover over a region');
+#       };
+#       info.addTo(map);
+#       };
+#       
+#       // mouse events
+#       layer.on({
+#       
+#       // mouseover to highlightFeature
+#       mouseover: function (e) {
+#       var layer = e.target;
+#       layer.setStyle({
+#       strokeWidth: '3px',
+#       strokeOpacity: 0.7,
+#       fillOpacity: 0.5
+#       });
+#       if (!L.Browser.ie && !L.Browser.opera) {
+#       layer.bringToFront();
+#       }
+#       info.update(layer.feature.properties);
+#       },
+#       
+#       // mouseout to resetHighlight
+#       mouseout: function (e) {
+#       geojsonLayer.resetStyle(e.target);
+#       info.update();
+#       },
+#       
+#       // click to zoom
+#       click: function (e) {
+#       var layer = e.target;        
+#       if ( feature.geometry.type === 'MultiPolygon' ) {        
+#       // for multipolygons get true extent
+#       var bounds = layer.getBounds(); // get the bounds for the first polygon that makes up the multipolygon
+#       // loop through coordinates array, skip first element as the bounds var represents the bounds for that element
+#       for ( var i = 1, il = feature.geometry.coordinates[0].length; i < il; i++ ) {
+#       var ring = feature.geometry.coordinates[0][i];
+#       var latLngs = ring.map(function(pair) {
+#       return new L.LatLng(pair[1], pair[0]);
+#       });
+#       var nextBounds = new L.LatLngBounds(latLngs);
+#       bounds.extend(nextBounds);
+#       }
+#       map.fitBounds(bounds);
+#       } else {
+#       // otherwise use native target bounds
+#       map.fitBounds(e.target.getBounds());
+#       }
+#       }
+#       });
+#       } !#")
     legend_vec = c('Red'='Blood', 'Green'='Nature', 'Yellow'='Sun')
     lmap$legend(position = 'bottomright', 
                 colors   =  names(legend_vec), 
@@ -366,21 +366,23 @@ Atlas_i6_SpeciesMap <- function(df,
   #fisrt subset by species
   for (species.current in unique(df$species)) {
     
-    if (withSparql) {      
-      #get species scientific name from ecoscope sparql
-      sparqlResult <- getSpeciesFromEcoscope(as.character(species.current))
-      
-      if (length(sparqlResult) > 0) {
-        species.label <- sparqlResult[1,"scientific_name"]
-        species.URI <- sparqlResult[1,"uri"]
-      } else {
-        species.label <- species.current
-        species.URI <- species.current
-      } 
-    } else {
-      species.label <- species.current
-      species.URI <- species.current
-    }
+#     if (withSparql) {      
+#       #get species scientific name from ecoscope sparql
+#       sparqlResult <- getSpeciesFromEcoscope(as.character(species.current))
+#       
+#       if (length(sparqlResult) > 0) {
+#         species.label <- sparqlResult[1,"scientific_name"]
+#         species.URI <- sparqlResult[1,"uri"]
+#       } else {
+#         species.label <- species.current
+#         species.URI <- species.current
+#       } 
+#     } else {
+#       species.label <- species.current
+#       species.URI <- species.current
+#     }
+    species.label <- species.current
+    species.URI <- species.current
     
     species.df <- df[df$species == species.current,]
     
@@ -390,46 +392,46 @@ Atlas_i6_SpeciesMap <- function(df,
     one <- plotFct(species.df, species.label, species.current, tableauResult, store)
     #     buildRdf <- function(store, tableauResult, RDFMetadata, rdf_file_path, rdf_subject, titles=c(), descriptions=c(), subjects=c(), tabURIs, processes=c(), image, data_output_identifiers, download, start=NA, end=NA, spatial=NA, withSparql=TRUE) {
     
-    tableauResult <- buildRdf(store,
-                              one$tableauResult,
-                              one$RDFMetadata,
-                              one$rdf_file_path,
-                              one$rdf_subject, 
-                              one$titles,
-                              one$descriptions,
-                              one$subjects,
-                              one$tabURIs,
-                              one$processes,
-                              one$image,
-                              one$data_output_identifiers,
-                              one$download,
-                              one$start,
-                              one$end,
-                              one$spatial,
-                              one$withSparql)
+#     tableauResult <- buildRdf(store,
+#                               one$tableauResult,
+#                               one$RDFMetadata,
+#                               one$rdf_file_path,
+#                               one$rdf_subject, 
+#                               one$titles,
+#                               one$descriptions,
+#                               one$subjects,
+#                               one$tabURIs,
+#                               one$processes,
+#                               one$image,
+#                               one$data_output_identifiers,
+#                               one$download,
+#                               one$start,
+#                               one$end,
+#                               one$spatial,
+#                               one$withSparql)
     
     #for each year
     if (length(unique(species.df$year)) > 1)
     {
       for(year.current in unique(species.df$year)) {
         one <- plotFct(species.df[species.df$year==year.current,], species.label, species.current, tableauResult, store)
-        tableauResult <- buildRdf(store,
-                                   one$tableauResult,
-                                   one$RDFMetadata,
-                                   one$rdf_file_path,
-                                   one$rdf_subject, 
-                                   one$titles,
-                                   one$descriptions,
-                                   one$subjects,
-                                   one$tabURIs,
-                                   one$processes,
-                                   one$image,
-                                   one$data_output_identifiers,
-                                   one$download,
-                                   one$start,
-                                   one$end,
-                                   one$spatial,
-                                   one$withSparql)
+#         tableauResult <- buildRdf(store,
+#                                    one$tableauResult,
+#                                    one$RDFMetadata,
+#                                    one$rdf_file_path,
+#                                    one$rdf_subject, 
+#                                    one$titles,
+#                                    one$descriptions,
+#                                    one$subjects,
+#                                    one$tabURIs,
+#                                    one$processes,
+#                                    one$image,
+#                                    one$data_output_identifiers,
+#                                    one$download,
+#                                    one$start,
+#                                    one$end,
+#                                    one$spatial,
+#                                    one$withSparql)
         
       }
       
@@ -440,23 +442,23 @@ Atlas_i6_SpeciesMap <- function(df,
         for(decade.current in unique(species.df$decade)) {
           
           one <- plotFct(species.df[species.df$decade==decade.current,], species.label, species.current, tableauResult, store)
-          tableauResult <- buildRdf(store,
-                                     one$tableauResult,
-                                     one$RDFMetadata,
-                                     one$rdf_file_path,
-                                     one$rdf_subject, 
-                                     one$titles,
-                                     one$descriptions,
-                                     one$subjects,
-                                     one$tabURIs,
-                                     one$processes,
-                                     one$image,
-                                     one$data_output_identifiers,
-                                     one$download,
-                                     one$start,
-                                     one$end,
-                                     one$spatial,
-                                     one$withSparql)
+#           tableauResult <- buildRdf(store,
+#                                      one$tableauResult,
+#                                      one$RDFMetadata,
+#                                      one$rdf_file_path,
+#                                      one$rdf_subject, 
+#                                      one$titles,
+#                                      one$descriptions,
+#                                      one$subjects,
+#                                      one$tabURIs,
+#                                      one$processes,
+#                                      one$image,
+#                                      one$data_output_identifiers,
+#                                      one$download,
+#                                      one$start,
+#                                      one$end,
+#                                      one$spatial,
+#                                      one$withSparql)
           
         }
       }
@@ -467,8 +469,10 @@ Atlas_i6_SpeciesMap <- function(df,
   
   
   # Packing the description of results in Json file storing all metadata (same as RDF)  
-  julien<-buildJson(type="map", description="Rapport d'exécution du traitement i6",processSourceCode="http://mdst-macroes.ird.fr:8084/wps/R/Atlas_i6_SpeciesMap.R",results=tableauResult)
-  return(julien)
+# julien<-buildJson(type="map", description="Rapport d'exécution du traitement i6",processSourceCode="http://mdst-macroes.ird.fr:8084/wps/R/Atlas_i6_SpeciesMap.R",results=tableauResult)
+
+julien<-'juli'
+return(julien)
   
   
   }

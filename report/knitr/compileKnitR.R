108c3af53757b9fc600390d0a3e91d24c29f3b54
juldebar <juldebar@gmail.com>
2015-11-24 18:35:02 +0100

    modofocation compilation knitR

--- report/knitr/compileKnitR.R
+++ report/knitr/compileKnitR.R
@@ -17,38 +17,3 @@ mywd <- '/home/julien/SVNs/GIT/IRDTunaAtlas/'
 file.inout <-'ICCAT_Report_BFTE'
 
 Run_Report(path.inout = mywd,file.inout =file.inout,show.pdf=F)
-
-Run_Report <- function(path.inout,file.inout,show.pdf=F){
-
-report_subdirectory <- paste(mywd,'report/knitr/',sep="")
-setwd(report_subdirectory)
-
-#Creating variables for names of files requires to be compiled
-file.in <- paste(file.inout,".Rnw",sep="")
-file.out <- paste(file.inout,".tex",sep="")
-# report_parametrization_file
-file.input.par <- paste(file.inout,".R",sep="")
-
-#### Check existence of .R and .Rnw fields
-if(!file.exists(paste(report_subdirectory,file.input.par,sep="/"))){ stop("You have to create the .R field.")}
-if(!file.exists(paste(report_subdirectory,file.in,sep="/"))){ stop("You have to create the .Rnw field.")}
-##
-report_parametrization_file<-paste(report_subdirectory,file.input.par,sep="")
-# source(report_parametrization_file)
-source(report_parametrization_file)
-
-#Specify the names of knitr file to be compiled and resulting latex file name (knitr output)
-setwd(report_subdirectory)
-
-# system(paste("unzip ",zipfile, " ./", sep=""))
-#knitr Compilation
-knit(file.in,file.out) 
-#Latex Compilation
-system(paste("pdflatex ",file.out, sep=""))
-system(paste("pdflatex ",file.out, sep=""))
-system(paste("pdflatex ",file.out, sep=""))
-if(show.pdf){
-  system(paste("acroread ",file.inout,".pdf",sep=""))
-}
-
-}

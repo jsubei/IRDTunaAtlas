6ef3b3b7f8550863ab0d0499a226f0d955d889f8
juldebar <juldebar@gmail.com>
2021-07-21 16:06:04 +0200

    manage dynamic content for filters / inputs

--- shiny/sardara_shiny_i6i7i8/app.R
+++ shiny/sardara_shiny_i6i7i8/app.R
@@ -24,49 +24,51 @@ source("https://raw.githubusercontent.com/juldebar/IRDTunaAtlas/master/R/TunaAtl
 source("https://raw.githubusercontent.com/juldebar/IRDTunaAtlas/master/R/wkt2spdf.R")
 ####################################################################################################################################################################################################################################
 DRV=RPostgres::Postgres()
-source(file = "~/Desktop/CODES/IRDTunaAtlas/credentials.R")
+con <- dbConnect(drv=DRV, dbname="tunaatlas_210715", user="tunaatlas_u", password="21c0551e7ed2911", host="localhost")
+# source(file = "~/Desktop/CODES/IRDTunaAtlas/credentials.R")
 ####################################################################################################################################################################################################################################
+
+
 new_wkt <- 'POLYGON((-180 -90, 180 -90, 180 90, -180 90, -180 -90))'
 wkt <- reactiveVal(new_wkt) 
 # target_species<- c("YFT","BFT")
 # target_year <- c(seq(1:10)+1994)
 # target_flag <- c("331","747")
-
-# query_i6i7i8 =  paste0("SELECT ogc_fid, geom_id, geom, year, species, country, value, count FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",new_wkt,"',4326)) AND species IN ('",paste0(target_species,collapse="','"),"') AND country IN ('",paste0(target_flag,collapse="','"),"') AND year IN ('",paste0(target_year,collapse="','"),"');")
-query_i6i7i8 =  paste0("SELECT ogc_fid, geom_id, geom, year, species, country, value, count FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",new_wkt,"',4326));")
-df_i6i7i8 = st_read(con, query = query_i6i7i8)
-
-# species_i8 = unique(df_i6i7i8$year)
-# year_i8 = unique(df_i6i7i8$species)
-
+target_species <- dbGetQuery(con, "SELECT DISTINCT(species) FROM public.i6i7i8;")
+target_year <- dbGetQuery(con, "SELECT DISTINCT(year) FROM public.i6i7i8;")
+target_flag <- dbGetQuery(con, "SELECT DISTINCT(country) FROM public.i6i7i8;")
+default_species <- 'YFT'
+default_year <- '2015'
+default_flag <- '322'
+filters_combinations <- dbGetQuery(con, "SELECT species, year, country FROM  public.i6i7i8 GROUP BY species, year, country;")
 
 ui <- fluidPage(
-    titlePanel("test Tuna"),
+    titlePanel("Tuna Atlas: indicateurs cartographiques"),
     sidebarLayout(
         sidebarPanel(
             selectInput(
                 inputId = "species_i6i7i8",
                 label = "Species",
-                choices = unique(df_i6i7i8$species),
-                selected="YFT"
+                choices = target_species$species,
+                selected= default_species
             ),
             selectInput(
                 inputId = "year_i8",
                 label = "Year",
-                choices = unique(df_i6i7i8$year),
-                selected="2015"
+                choices = target_year$year,
+                selected= default_year
             ),
             selectInput(
                 inputId = "flag_i8",
                 label = "Flag",
-                choices = unique(df_i6i7i8$country),
-                selected="322"
+                choices = target_flag$country,
+                selected= default_flag
             ),
             selectInput(
               inputId = "species_i8",
               label = "Additionnal species (only required for i8)",
-              choices = unique(df_i6i7i8$species),
-              selected=c("YFT","BET","SKJ"),
+              choices = target_species$species[-which(target_species$species %in% default_species)],
+              selected=c("BET","SKJ"),
               multiple=TRUE
             ),
             actionButton(
@@ -77,13 +79,12 @@ ui <- fluidPage(
         ),
         # mainPanel(
         #     # tableOutput("tbl"),
-        #     leafletOutput('mymap') 
+        #     leafletOutput('mymap')
         # ),
         tabsetPanel(
             tabPanel(
                 title = "Map data",
                 leafletOutput('mymap', width = "50%", height = 600)
-                # leafletOutput('mymap')
             ),
             tabPanel(
               title = "Plot indicator 6",
@@ -98,8 +99,16 @@ ui <- fluidPage(
               plotlyOutput("plot8")
             ),
             tabPanel(
-                title = "Browse data",
-                tableOutput("tbl")
+                title = "Browse i6i7 data",
+                DT::dataTableOutput("DTi6i7")
+            ),
+            tabPanel(
+              title = "Browse i8 table",
+              DT::dataTableOutput("DTi8")
+            ),
+            tabPanel(
+              title = "Your SQL query",
+              textOutput("sql_query")
             ),
             tabPanel(
                 title = "Your filters",
@@ -111,31 +120,110 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  
+
+    sql_query <- eventReactive(input$submit, {
+      #query =  paste0("SELECT ogc_fid, geom_id, geom, year, species, country, value, count FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) AND species IN ('",paste0(input$species_i6i7i8,collapse="','"),"') AND country IN ('",paste0(input$flag_i8,collapse="','"),"') AND year IN ('",paste0(input$year_i8,collapse="','"),"');")
+    paste0("SELECT  ogc_fid, geom_id, geom, year, species, country, value, count,ST_asText(geom) AS geom_wkt FROM public.i6i7i8 
+                        WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) 
+                        AND species IN ('",paste0(append(input$species_i8,input$species_i6i7i8),collapse="','"),"') 
+                        AND country IN ('",paste0(input$flag_i8,collapse="','"),"') 
+                        AND year IN ('",paste0(input$year_i8,collapse="','"),"');")
+    },
+    ignoreNULL = FALSE)
     
+  data <- eventReactive(input$submit, {
+    df <- as.data.frame(st_read(con, query = sql_query())) %>% mutate(time_start = ISOdate(year, 1, 1), time_end = ISOdate(year, 12,31)) %>% dplyr::select (-c(ogc_fid, geom_id,geom,count,year)) %>%  dplyr::rename(v_catch=value,flag=country)
+  },
+  # on.exit(dbDisconnect(conn), add = TRUE)
+  ignoreNULL = FALSE)
+  
+  
+  change <- reactive({
+    unlist(strsplit(paste(c(input$species_i6i7i8,input$year_i8,input$flag_i8),collapse="|"),"|",fixed=TRUE))
+  })
+  
+  observeEvent(input$resetWkt, {
+    wkt(new_wkt)
+    
+  })
+  
+  observeEvent(input$species_i6i7i8,{
+    # temp <- filters_combinations %>% filter(grepl(pattern ="BFT",comb))
+    temp <- filters_combinations %>% filter(species %in% change()[1])
+    # updateSelectInput(session,"year_i8",choices = unique(temp$year),selected = temp$year[1])
+    # updateSelectInput(session,"flag_i8",choices = unique(temp$country),selected = temp$country[1])
+    updateSelectInput(session,"year_i8",choices = unique(temp$year))
+    # updateSelectInput(session,"flag_i8",choices = unique(temp$country))
+  }
+  )
+
+  observeEvent(input$year_i8,{
+    temp <- filters_combinations %>% filter(species %in% change()[1], year %in% change()[2])
+    # updateSelectInput(session,"species_i6i7i8",choices = unique(temp$species),selected = temp$species[1])
+    # updateSelectInput(session,"flag_i8",choices = unique(temp$country),selected = temp$country[1])
+    # updateSelectInput(session,"species_i6i7i8",choices = unique(temp$species))
+    updateSelectInput(session,"flag_i8",choices = unique(temp$country))
+  }
+  )
+
+  # observeEvent(input$flag_i8,{
+  #   temp <- filters_combinations%>% filter(country %in% change()[3] & year %in% change()[2])
+  #   updateSelectInput(session,"species_i6i7i8",choices = unique(temp$species),selected = temp$species[1])
+  #   updateSelectInput(session,"year_i8",choices = unique(temp$year),selected = temp$year[1])
+  # }
+  # )
+  # 
+  # observeEvent(input$flag_i8,{  
+  #   temp <- dat %>% filter(id1 %in% change()[1] & id2 %in% change()[2])
+  #   updateSelectInput(session,"id3",choices = unique(temp$id3))
+  # }
+  # )
+  
     output$selected_var <- renderText({ 
-        paste("You have selected", input$species_i6i7i8, "and", wkt())
+        paste("You have selected:\n", input$species_i6i7i8, "and \n", input$year_i8, "and \n", input$flag_i8, "and \n", wkt())
     })
     
-    data <- eventReactive(input$submit, {
-        conn <-con
-        query <- paste0("SELECT year, species, country, value, ST_asText(geom) AS geom_wkt FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) AND species IN ('",paste0(input$species_i6i7i8,collapse="','"),"') AND country IN ('",paste0(input$flag_i8,collapse="','"),"') AND year IN ('",paste0(input$year_i8,collapse="','"),"');")
-        # query <- paste0("SELECT species, country AS flag, value AS v_catch, ST_asText(geom) AS geom_wkt, (year || '-01-01') AS time_start, (year || '-12-31') AS time_end FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) AND species IN ('",paste0(input$species_i8,collapse="','"),"') AND country IN ('",paste0(input$flag_i8,collapse="','"),"') AND year IN ('",paste0(input$year_i8,collapse="','"),"');")
-        # df <- st_read(con, query = query) %>% mutate(time_start = ISOdate(year, 1, 1), time_end = ISOdate(year, 12,31)) %>% dplyr::select (-c(ogc_fid, geom_id,count,year)) %>%  dplyr::rename(v_catch=value,flag=country,geom_wkt=geom)
-        df <- st_read(con, query = query) %>% mutate(time_start = ISOdate(year, 1, 1), time_end = ISOdate(year, 12,31)) %>% dplyr::select (-c(year)) %>%  dplyr::rename(v_catch=value,flag=country)
-    }, ignoreNULL = FALSE)
+
+    output$sql_query <- renderText({ 
+      paste("Your SQL Query is : \n", sql_query())
+    })
         
      
-    output$tbl <- renderTable({
-
-        this <- data()
-
-        # on.exit(dbDisconnect(conn), add = TRUE)
-        
+    output$DTi6i7 <- renderDT({
+      this <- data() %>% filter(species %in% input$species_i6i7i8)
     })
     
 
+    output$DTi8 <- renderDT({
+      this <- data()
+    })
+
+    # output$dataTable = DT::renderDataTable({
+    #   this <- data()
+    # })
+    
+    # output$dataTable <-DT::renderDT({
+    #   DT::datatable(
+    #     data(),
+    #     selection = "none",
+    #     extensions = 'Buttons',
+    #     option = list(
+    #       buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
+    #       dom = 'Brti'
+    #     )
+    #   ) %>%
+    #     DT::formatCurrency(
+    #       columns = 2:5,
+    #       currency = '',
+    #       mark = " ",
+    #       digits = 0
+    #     )
+    # })
+    
+    
     output$mymap <- renderLeaflet({
-        df <- data()
+        df <- data() %>% filter(species %in% input$species_i6i7i8)
         netCDF_file <- TunaAtlas_database_to_NetCDF(df)
         centroid <- st_union(st_as_sfc(df$geom_wkt)) %>% st_convex_hull()   %>% st_centroid()
         lat_centroid <- st_coordinates(centroid)[2]
@@ -206,15 +294,11 @@ server <- function(input, output, session) {
     output$plot7 <- renderPlotly({
         
         
-      #Filtering input dataset with selected parameters
-      if(wkt()!=new_wkt){
-            # df_i6i7 = st_read(con,query =  paste0("SELECT ogc_fid, geom_id, geom, year, species, country, value, count FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) AND species IN ('",paste0(input$species_i8,collapse="','"),"') AND country IN ('",paste0(input$flag_i8,collapse="','"),"') AND year IN ('",paste0(input$year_i8,collapse="','"),"');"))
-            df_i6i7=df_i6i7i8 %>% filter(st_within(geom,st_as_sfc(wkt(), crs = 4326)) %>% lengths > 0, year %in% input$year_i8, species %in% input$species_i6i7i8, country %in% input$flag_i8 ) 
-        }else{
-          df_i6i7=df_i6i7i8 %>% filter(year %in% input$year_i8, species %in% input$species_i6i7i8,country %in% input$flag_i8) 
-        }
-        
+
+      
+        df_i6i7 = st_read(con,query = sql_query())  %>% filter(species %in% input$species_i6i7i8)
         df_i7_filtered <- as(df_i6i7, "Spatial")
+        
         i7 <-  Atlas_i7_SpeciesMapRelativeCatches(df=df_i7_filtered, 
                                                   geomIdAttributeName="geom_id",
                                                   yearAttributeName="year", 
@@ -228,18 +312,11 @@ server <- function(input, output, session) {
     
     output$plot6 <- renderPlotly({ 
       
-      if(wkt()!=new_wkt){
-        
-        df_i6i7 = st_read(con, 
-                          query =  paste0("SELECT ogc_fid, geom_id, geom, year, species, country, value, count FROM public.i6i7i8 WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326)) AND species IN ('",paste0(input$species_i6i7i8,collapse="','"),"') AND country IN ('",paste0(input$flag_i8,collapse="','"),"') AND year IN ('",paste0(input$year_i8,collapse="','"),"');")
-        )
-      }else{
-        df_i6i7=df_i6i7i8 %>% filter(year %in% input$year_i8, species %in% input$species_i6i7i8,country %in% input$flag_i8) 
-      }
       
+      df_i6i7 = st_read(con,query = sql_query())  %>% filter(species %in% input$species_i6i7i8)
       df_i6_filtered <- as(df_i6i7, "Spatial")
       
-      withProgress(message = 'Making plot i6', value = 0, {
+      # withProgress(message = 'Making plot i6', value = 0, {
       i6 <- Atlas_i6_SpeciesMap(df=df_i6_filtered,
                                 geomIdAttributeName="geom_id",
                                 yearAttributeName="year",
@@ -247,21 +324,18 @@ server <- function(input, output, session) {
                                 valueAttributeName="value",
                                 withSparql=FALSE
       )
-      })
+      # })
       
     })
     
 
     output$plot8 <- renderPlotly({
-
-        if(wkt()!=new_wkt){
-            df_i8=df_i6i7i8 %>% filter(st_within(geom,st_as_sfc(wkt(), crs = 4326)) %>% lengths > 0, year %in% input$year_i8, species %in% input$species_i8, country %in% input$flag_i8 ) 
-        }else{
-            df_i8=df_i6i7i8 %>% filter(year %in% input$year_i8, species %in% input$species_i8,country %in% input$flag_i8) 
-        }
-
-        df_i8_filtered <- as(df_i8, "Spatial")
-        i8 <- Atlas_i8_SpeciesMapRelativeCatchesOtherSpecies(df=df_i8_filtered,
+      
+      # df_i8=df_i6i7i8 %>% filter(st_within(geom,st_as_sfc(wkt(), crs = 4326)) %>% lengths > 0, year %in% input$year_i8, species %in% append(input$species_i8,input$species_i6i7i8), country %in% input$flag_i8 ) 
+      df_i8 = st_read(con,query = sql_query()) 
+      df_i8_filtered <- as(df_i8, "Spatial")
+      
+      i8 <- Atlas_i8_SpeciesMapRelativeCatchesOtherSpecies(df=df_i8_filtered,
                                                              targetedSpecies=input$species_i6i7i8,
                                                              geomIdAttributeName="geom_id",
                                                              yearAttributeName="year",
@@ -274,14 +348,6 @@ server <- function(input, output, session) {
     })
 
     
-    
-    
-    
-    observeEvent(input$resetWkt, {
-        wkt(new_wkt)
-        
-    })
-
 }
 
 

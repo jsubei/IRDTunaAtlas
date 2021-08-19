e0781088e9388e8038fb26f14e8fe0b87ed80820
juldebar <juldebar@gmail.com>
2021-08-19 10:50:25 +0200

    display indicator as an image and improve map overview, SQL query with glue package..

--- shiny/sardara_shiny_i4/app.R
+++ shiny/sardara_shiny_i4/app.R
@@ -20,11 +20,12 @@ library(streamgraph)
 library(viridis)
 
 ####################################################################################################################################################################################################################################
-source("https://raw.githubusercontent.com/juldebar/IRDTunaAtlas/master/R/TunaAtlas_i4_SpeciesMonthByOcean.R")
+# source("https://raw.githubusercontent.com/juldebar/IRDTunaAtlas/master/R/TunaAtlas_i4_SpeciesMonthByOcean.R")
+source("/home/julien/Desktop/CODES/IRDTunaAtlas/R/TunaAtlas_i4_SpeciesMonthByOcean.R")
 ####################################################################################################################################################################################################################################
 DRV=RPostgres::Postgres()
-# source(file = "~/Desktop/CODES/IRDTunaAtlas/credentials.R")
-source(file = "~/Bureau/CODES/IRDTunaAtlas/credentials.R")
+source(file = "~/Desktop/CODES/IRDTunaAtlas/credentials.R")
+# source(file = "~/Bureau/CODES/IRDTunaAtlas/credentials.R")
 ####################################################################################################################################################################################################################################
 
 new_wkt <- 'POLYGON((-180 -90, 180 -90, 180 90, -180 90, -180 -90))'
@@ -39,51 +40,58 @@ target_ocean <- dbGetQuery(con, "SELECT DISTINCT(ocean) FROM public.i4_spatial O
 selectInput("species_choice", label = h3("Select species for i4 "), choices = target_species, selected = c("SKJ","YFT","BET"),multiple = TRUE)
 selectInput("year_choice", label = h3("Select year for i4"), choices = target_year, selected = c("2000","2001","2002"),multiple = TRUE)
 
-default_species <-  c("SKJ","YFT","BET")
+default_species <-  c("SKJ")
+# default_species <-  c("SKJ","YFT")
 default_year <- c(seq(min(target_year):max(target_year))+min(target_year)-1)
+# default_year <- "c(seq(2000:2010))"
 default_ocean <- unique(target_ocean)
 filters_combinations <- dbGetQuery(con, "SELECT species, year, ocean FROM public.i4_spatial GROUP BY  species, year, ocean  ;")
 
 ui <- fluidPage(
   titlePanel("Tuna Atlas: indicateurs cartographiques"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "species",
-        label = "Species",
-        choices = target_species$species,
-        multiple = TRUE,
-        selected= default_species
-      ),
-      selectInput(
-        inputId = "year",
-        label = "Year",
-        choices = target_year$year,
-        multiple = TRUE,
-        selected= default_year
-      ),
-      selectInput(
-        inputId = "ocean",
-        label = "Ocean",
-        choices = target_ocean$ocean,
-        multiple = TRUE,
-        selected= target_ocean$ocean
-      ),
-      actionButton(
-        inputId = "submit",
-        label = "Submit"
-      ),
-      actionButton("resetWkt", "Reset WKT to global")
+  navbarPage(title="TunaAtlas", 
+             tabPanel("Interactive",
+                      
+                      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+                      leafletOutput('mymap', width = "60%", height = 1500),
+                      
+                      
+                      # Shiny versions prior to 0.11 should use class = "modal" instead.
+                      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                    draggable = TRUE, top = 200, left = "auto", right = 20, bottom = "auto",
+                                    width = "30%", height = "auto",
+                      selectInput(
+                        inputId = "species",
+                        label = "Species",
+                        choices = target_species$species,
+                        multiple = TRUE,
+                        selected= default_species
+                      ),
+                      selectInput(
+                        inputId = "year",
+                        label = "Year",
+                        choices = target_year$year,
+                        multiple = TRUE,
+                        selected= default_year
+                      ),
+                      selectInput(
+                        inputId = "ocean",
+                        label = "Ocean",
+                        choices = target_ocean$ocean,
+                        multiple = TRUE,
+                        selected= target_ocean$ocean
+                      ),
+                      actionButton(
+                        inputId = "submit",
+                        label = "Submit"
+                      ),
+                      actionButton("resetWkt", "Reset WKT to global"),
+                      tags$br(),
+                      imageOutput("plot4_image", width = "400", height = "300",inline = TRUE)
+                      
+                      )
     ),
-    # mainPanel(
-    #     # tableOutput("tbl"),
-    #     leafletOutput('mymap')
-    # ),
-    tabsetPanel(
-      tabPanel(
-        title = "Map Postgis Vector / SF data",
-        leafletOutput('mymap', width = "50%", height = 600)
-      ),
+
       tabPanel(
         title = "Plot indicator 4",
         # plotlyOutput("plot1")
@@ -103,25 +111,45 @@ ui <- fluidPage(
       )
     )
   )
-)
 
 
 server <- function(input, output, session) {
   
+  # 
+  # sql_query <- eventReactive(input$submit, {
+  #   
+  #   query = paste0("select species, year, month, ocean, sum(value) as value, geom FROM public.i4_spatial WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326))  AND species IN ('",paste0(input$species,collapse="','"),"')   AND year IN ('",paste0(input$year,collapse="','"),"')  GROUP BY  species, year, month, ocean ;")
+  # },
+  # 
+  # ignoreNULL = FALSE)
+  
   
   sql_query <- eventReactive(input$submit, {
-    
-    query = paste0("select species, year, month, ocean, sum(value) as value FROM public.i4_spatial WHERE ST_Within(geom,ST_GeomFromText('",wkt(),"',4326))  AND species IN ('",paste0(input$species,collapse="','"),"')   AND year IN ('",paste0(input$year,collapse="','"),"')  GROUP BY  species, year, month, ocean ;")
+    if(is.null(input$year)){year_name=target_year$year}else{year_name=input$year}
+    query <- glue::glue_sql("SELECT species, year, month, ocean, sum(value) as value, geom FROM public.i4_spatial 
+      WHERE ST_Within(geom,ST_GeomFromText(({wkt*}),4326)) 
+      AND  species IN ({species_name*}) 
+    AND year IN ({year_name*}) 
+                             GROUP BY  species, year, month, ocean, geom",
+      wkt = wkt(),
+      species_name = input$species,
+      year_name = year_name,
+      .con = con)
   },
-  
   ignoreNULL = FALSE)
   
+  
   data_i4 <- eventReactive(input$submit, {
-    st_read(con, query = sql_query()) %>% filter(year %in% input$year) #%>% group_by(ocean,year,month,species) %>% summarise(value = sum(value)) 
-    
+    st_read(con, query = paste0("SELECT species, year, month, ocean, sum(value) as value FROM(",sql_query(),") AS foo GROUP BY  species, year, month, ocean"))
   },
   # on.exit(dbDisconnect(conn), add = TRUE)
   ignoreNULL = FALSE)
+  
+  
+  metadata <- reactive({
+    st_read(con, query = paste0("SELECT geom, sum(value) AS value FROM(",sql_query(),") AS foo GROUP BY geom")) 
+  })  
+  
   
   
   change <- reactive({
@@ -174,7 +202,7 @@ server <- function(input, output, session) {
   
   output$mymap <- renderLeaflet({
     
-    df <- data()
+    df <- metadata()
     centroid <-  st_convex_hull(df)   %>% st_centroid()
     lat_centroid <- st_coordinates(centroid)[2]
     lon_centroid <- st_coordinates(centroid)[1]
@@ -193,7 +221,7 @@ server <- function(input, output, session) {
     map_leaflet <- leaflet()  %>%  
       addPolygons(data = df,
                   label = ~value,
-                  popup = ~paste0("Captures de",species,": ", round(value), " tonnes(t) et des brouettes"),
+                  popup = ~paste0("Catches for this species: ", round(value), " tonnes(t) et des brouettes"),
                   # fillColor = ~pal_fun(value),
                   fillColor = ~qpal(value),
                   fill = TRUE,
@@ -270,6 +298,30 @@ server <- function(input, output, session) {
                                         valueAttributeName="value")
   i4
   })
+  
+  
+  output$plot4_image <- renderImage({
+    df_i4_filtered <- data_i4()
+    
+    i4 <-  Atlas_i4_SpeciesMonthByOcean(df=df_i4_filtered, 
+                                        oceanAttributeName="ocean", 
+                                        yearAttributeName="year", 
+                                        monthAttributeName="month",
+                                        speciesAttributeName="species", 
+                                        valueAttributeName="value")
+    i4 
+    png(i4, width = 1600, height = 1200)
+    dev.off()
+    
+    # Return a list containing the filename
+    list(src = i4,
+         contentType = 'image/png',
+         width = 800,
+         height = 600,
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+  
   
   
   

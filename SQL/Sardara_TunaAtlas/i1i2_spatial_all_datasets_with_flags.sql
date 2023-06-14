d66fbb2d454b26b6d8b3dc6ac5025d421606c1f1
juldebar <juldebar@gmail.com>
2023-06-14 17:53:37 +0200

    update naming of columns

--- SQL/Sardara_TunaAtlas/i1i2_spatial_all_datasets_with_flags.sql
+++ SQL/Sardara_TunaAtlas/i1i2_spatial_all_datasets_with_flags.sql
@@ -1,8 +1,8 @@
--- View: public.i1i2_spatial_all_datasets
+-- View: public.i1i2_spatial_all_datasets_with_fishingfleets
 
--- DROP MATERIALIZED VIEW public.i1i2_spatial_all_datasets;
+-- DROP MATERIALIZED VIEW public.i1i2_spatial_all_datasets_with_fishingfleets;
 
-CREATE MATERIALIZED VIEW public.i1i2_spatial_all_datasets_with_flags
+CREATE MATERIALIZED VIEW public.i1i2_spatial_all_datasets_with_fishingfleets
 TABLESPACE pg_default
 AS
  SELECT metadata.identifier AS dataset,
@@ -29,13 +29,14 @@ AS
      LEFT JOIN species.species_labels USING (id_species)
      LEFT JOIN species.species_mapping ON species_mapping.species_mapping_id_from = tab.id_species
      LEFT JOIN species.species_labels speciesgroup_label ON speciesgroup_label.id_species = species_mapping.species_mapping_id_to
-  WHERE (metadata.identifier = ANY (ARRAY['global_nominal_catch_firms_level0'::text, 'global_catch_firms_level0'::text, 'global_catch_5deg_1m_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_ird_level1'::text, 'global_catch_1deg_1m_ps_bb_ird_level2'::text, 'global_catch_5deg_1m_ird_level1'::text, 'global_catch_5deg_1m_ird_level2'::text,  'global_catch_ird_level1'::text, 'global_catch_ird_level2'::text] )) 
+  WHERE 
+  (metadata.identifier = ANY (ARRAY['global_nominal_catch_firms_level0'::text, 'global_catch_firms_level0'::text, 'global_catch_5deg_1m_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_ird_level1'::text, 'global_catch_1deg_1m_ps_bb_ird_level2'::text, 'global_catch_5deg_1m_ird_level1'::text, 'global_catch_5deg_1m_ird_level2'::text,  'global_catch_ird_level1'::text, 'global_catch_ird_level2'::text] )) 
   	AND metadata.id_metadata = tab.id_metadata
   GROUP BY metadata.identifier, source_labels.codesource_source, geargroup_label.codesource_gear,fishingfleet_labels.source_label, "time".year, species_labels.codesource_species, unit_labels.codesource_unit, area_labels.geom
 WITH DATA;
 
-ALTER TABLE public.i1i2_spatial_all_datasets
+ALTER TABLE public.i1i2_spatial_all_datasets_with_fishingfleets
     OWNER TO tunaatlas_u;
 
-GRANT ALL ON TABLE public.i1i2_spatial_all_datasets TO tunaatlas_u;
-GRANT SELECT ON TABLE public.i1i2_spatial_all_datasets TO tunaatlas_inv;
+GRANT ALL ON TABLE public.i1i2_spatial_all_datasets_with_fishingfleets TO tunaatlas_u;
+GRANT SELECT ON TABLE public.i1i2_spatial_all_datasets_with_fishingfleets TO tunaatlas_inv;

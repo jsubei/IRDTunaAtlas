7699b9223fb9e8756fb9fabcbf08fd2e51a9bafe
Julien Barde <juldebar@users.noreply.github.com>
2024-02-21 09:32:07 +0100

    Update i1i2_spatial_all_datasets_with_flags.sql
    new SQL model mapping

--- SQL/Sardara_TunaAtlas/i1i2_spatial_all_datasets_with_flags.sql
+++ SQL/Sardara_TunaAtlas/i1i2_spatial_all_datasets_with_flags.sql
@@ -5,6 +5,7 @@
 CREATE MATERIALIZED VIEW public.i1i2_spatial_all_datasets_with_fishingfleets
 TABLESPACE pg_default
 AS
+
  SELECT metadata.identifier AS dataset,
     source_labels.codesource_source AS ocean,
     "time".year,
@@ -33,6 +34,7 @@ AS
   (metadata.identifier = ANY (ARRAY['global_nominal_catch_firms_level0'::text, 'global_catch_firms_level0'::text, 'global_catch_5deg_1m_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_firms_level0'::text, 'global_catch_1deg_1m_ps_bb_ird_level1'::text, 'global_catch_1deg_1m_ps_bb_ird_level2'::text, 'global_catch_5deg_1m_ird_level1'::text, 'global_catch_5deg_1m_ird_level2'::text,  'global_catch_ird_level1'::text, 'global_catch_ird_level2'::text] )) 
   	AND metadata.id_metadata = tab.id_metadata
   GROUP BY metadata.identifier, source_labels.codesource_source, geargroup_label.codesource_gear,fishingfleet_labels.source_label, "time".year, species_labels.codesource_species, unit_labels.codesource_unit, area_labels.geom
+ 
 WITH DATA;
 
 ALTER TABLE public.i1i2_spatial_all_datasets_with_fishingfleets

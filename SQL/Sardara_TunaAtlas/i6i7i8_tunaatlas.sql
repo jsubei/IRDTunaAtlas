9cf29cb1d04bfedf95243085f390a508c249144e
Julien Barde <juldebar@users.noreply.github.com>
2024-03-05 16:34:15 +0100

    Update i6i7i8_tunaatlas.sql
    add measurement unit

--- SQL/Sardara_TunaAtlas/i6i7i8_tunaatlas.sql
+++ SQL/Sardara_TunaAtlas/i6i7i8_tunaatlas.sql
@@ -5,7 +5,6 @@
 CREATE MATERIALIZED VIEW public.i6i7i8
 TABLESPACE pg_default
 AS
-
 SELECT 
 	row_number() OVER () AS ogc_fid,
 	metadata.identifier AS dataset,
@@ -14,6 +13,7 @@ SELECT
 	fishing_fleet_labels.codesource_fishing_fleet AS fishing_fleet,
 	sum(fact.measurement_value) AS measurement_value,
 	count(fact.measurement_value) AS count,
+    measurement_unit_labels.codesource_measurement_unit AS unit,
 	area.codesource_area,
 	cwp_grid.geom,
 	fact.id_area AS geom_id
@@ -31,6 +31,8 @@ FROM metadata.metadata, fact_tables.catch fact
 	LEFT JOIN fishing_fleet.fishing_fleet_labels USING (id_fishing_fleet)
 	LEFT JOIN fishing_fleet.fishing_fleet_mapping_view ON fishing_fleet_mapping_view.db_idsource = fact.id_fishing_fleet
 	LEFT JOIN fishing_fleet.fishing_fleet_labels fishing_fleetgroup_label ON fishing_fleetgroup_label.id_fishing_fleet = fishing_fleet_mapping_view.db_idtarget
+	
+     LEFT JOIN measurement_unit.measurement_unit_labels USING (id_measurement_unit)
 
 WHERE
   metadata.identifier = ANY(ARRAY['global_catch_5deg_1m_firms_level0'::text,'global_catch_5deg_1m_firms_level1'::text,'global_catch_5deg_1m_ird_level2'::text,'global_catch_ird_level2'::text,
@@ -40,8 +42,7 @@ WHERE
   AND 
   cwp_grid.gridtype = '5deg_x_5deg'::text 
   
-GROUP BY metadata.identifier, fact.id_area, area.codesource_area, "time".year, species_labels.codesource_species, fishing_fleet_labels.codesource_fishing_fleet, cwp_grid.geom
-	
+GROUP BY metadata.identifier, fact.id_area, area.codesource_area, "time".year, species_labels.codesource_species, fishing_fleet_labels.codesource_fishing_fleet, cwp_grid.geom, measurement_unit_labels.codesource_measurement_unit 
 WITH DATA;
 
 ALTER TABLE public.i6i7i8
